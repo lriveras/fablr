e696ed519aff1e82c9269b2d659ddb6864a398bb
Luis Alberto <lriveras@gmail.com>
2017-01-15 03:24:33 -0500

    removing samples

--- app/features/sample-feature/sample-feature-reducer.js
+++
@@ -1,9 +0,0 @@
-const appBarReducer = (state, action)=>{
-        if(action.type==='LOGIN') 
-            return Object.assign({},state, {logged:true, session:action.session});
-        else if(action.type==='LOGOUT') 
-            return Object.assign({},state, {logged:false, session:null});
-        else return Object.assign({}, state);
-    };
-
-export default appBarReducer;
