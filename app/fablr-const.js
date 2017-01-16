82c31c7780675a99b71688e16dab6b39b2fcc942
Luis Alberto <lriveras@gmail.com>
2017-01-16 02:13:26 -0500

    setting appid for heroku

--- app/fablr-const.js
+++ app/fablr-const.js
@@ -1,5 +1,5 @@
 const FablrConst = {
-    appId: "1816468645258885",
+    appId: "354770958242800",
     appScope: "public_profile,manage_pages,publish_pages,read_insights,pages_show_list,pages_manage_cta,pages_manage_instant_articles",
 }
 
